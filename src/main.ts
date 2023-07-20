0fb7b0865a658eeebd8976e133a6db198098fe52
Park-Jeong-Ki <jgpark1998@gmail.com>
2023-07-20 20:14:20 +0900

    enableCors 추가

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const serverConfig = config.get('server');
   const port = serverConfig.port;
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
   await app.listen(port);
   Logger.log(`Application running on port 3000`);
 }
